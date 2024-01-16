dffa3f3ed5fc651123cf970f5e4813e0e2c2600d
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-01-16 21:25:53 +0400

    Operator overload

---
+++ BCTFO-20-NC/SecondConsoleApp/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace SecondConsoleApp
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public static Student operator +(Student obj1, Student obj2)
+        {
+            Student result = new Student();
+            result.Score = obj1.Score + obj2.Score;
+
+            return result;
+        }
+
+        public static Student operator -(Student obj1, Student obj2)
+        {
+            Student result = new Student();
+            result.Score = obj1.Score - obj2.Score;
+
+            return result;
+        }
+
+        public static Student operator *(Student obj1, Student obj2)
+        {
+            Student result = new Student();
+            result.Score = obj1.Score - obj2.Score;
+
+            return result;
+        }
+
+        public static Student operator /(Student obj1, Student obj2)
+        {
+            Student result = new Student();
+            result.Score = obj1.Score - obj2.Score;
+
+            return result;
+        }
+
+        public static bool operator >(Student obj1, Student obj2)
+        {
+            var result = obj1.Score > obj2.Score;
+
+            return result;
+
+        }
+
+        public static bool operator <(Student obj1, Student obj2)
+        {
+            var result = obj1.Score < obj2.Score;
+
+            return result;
+        }
+
+
+        public static bool operator >=(Student obj1, Student obj2)
+        {
+            var result = obj1.Score >= obj2.Score;
+
+            return result;
+        }
+
+
+        public static bool operator <=(Student obj1, Student obj2)
+        {
+            var result = obj1.Score >= obj2.Score;
+
+            return result;
+        }
+
+        public static bool operator ==(Student obj1, Student obj2)
+        {
+            var result = obj1.Score == obj2.Score && obj1.Name == obj2.Name;
+
+            return result;
+        }
+
+        public static bool operator !=(Student obj1, Student obj2)
+        {
+            var result = obj1.Score != obj2.Score || obj1.Name != obj2.Name;
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Score}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
