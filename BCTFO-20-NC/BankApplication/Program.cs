897214cc295e142e6260e2159dce63590576c385
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-02-13 21:43:29 +0400

    XML

--- BCTFO-20-NC/BankApplication/Program.cs
+++ BCTFO-20-NC/BankApplication/Program.cs
@@ -13,7 +13,7 @@ namespace BankApplication
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1(new CustomerJSONRepository()));
+            Application.Run(new Form1(new CustomerXMLRepository()));
         }
     }
 }
--- BCTFO-20-NC/BankApplication/Repository/CustomerXMLRepository.cs
+++ BCTFO-20-NC/BankApplication/Repository/CustomerXMLRepository.cs
@@ -1,18 +1,70 @@
 ﻿using BankApplication.Models;
 using BankApplication.Repository.Interfaces;
+using System.Xml;
 
 namespace BankApplication.Repository
 {
     public class CustomerXMLRepository : IRepository
     {
+        private const string _fileLocation = "C:\\Users\\User\\Desktop\\IT STEP\\BCTFO-20-NC\\BCTFO-20-NC\\BankApplication\\Customers.xml";
+        private List<Customer> _data = new();
+
+        public CustomerXMLRepository()
+        {
+            _data = Parse(File.ReadAllText(_fileLocation));
+        }
+
+        private static List<Customer> Parse(string input)
+        {
+            List<Customer> result = new();
+
+            XmlDocument xDoc = new();
+            xDoc.LoadXml(input);
+
+            XmlNodeList rowNodes = xDoc.SelectNodes("//row");
+
+            foreach (XmlNode rowNode in rowNodes)
+            {
+                Customer customer = new();
+
+                customer.Id = int.Parse(rowNode.SelectSingleNode("Id").InnerText);
+                customer.Name = rowNode.SelectSingleNode("Name").InnerText;
+                customer.IdentityNumber = rowNode.SelectSingleNode("IdentityNumber").InnerText;
+                customer.PhoneNumber = rowNode.SelectSingleNode("PhoneNumber").InnerText;
+                customer.Email = rowNode.SelectSingleNode("Email").InnerText;
+                customer.Type = int.Parse(rowNode.SelectSingleNode("Type").InnerText);
+
+                result.Add(customer);
+            }
+
+            return result;
+        }
+
         public List<Customer> GetAllCustomers()
         {
-            throw new NotImplementedException();
+            if (_data.Count <= 0)
+            {
+                throw new Exception("Data storage is empty");
+            }
+
+            return _data;
         }
 
         public Customer GetSingleCustomer(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+            {
+                throw new ArgumentNullException("Invalid argument passed");
+            }
+
+            var result = _data.FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
+            {
+                throw new NullReferenceException("Item not found");
+            }
+
+            return result;
         }
     }
 }
