a78f2533bb112670ed9c59eed0a5180f589dd21a
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-03-29 22:06:12 +0400

    MVC project added

---
+++ BCTFO-20-NC/HotelProject.Web/Controllers/HotelsController.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Controllers
+{
+    public class HotelsController : Controller
+    {
+        private readonly HotelRepository _hotelRepository;
+        public HotelsController()
+        {
+            _hotelRepository = new HotelRepository();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _hotelRepository.GetHotels();
+            return View(result);
+        }
+    }
+}
