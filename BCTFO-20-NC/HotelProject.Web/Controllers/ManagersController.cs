35edee9ae85ae633469d60155f8f26deeec09059
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-03-31 22:13:41 +0400

    Managers

--- BCTFO-20-NC/HotelProject.Web/Controllers/ManagersController.cs
+++ BCTFO-20-NC/HotelProject.Web/Controllers/ManagersController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.Web.Controllers
 {
     public class ManagersController : Controller
     {
-        public IActionResult Index()
+        private readonly ManagerRepository _managerRepository;
+        public ManagersController()
         {
-            return View();
+            _managerRepository = new();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _managerRepository.GetManagers();
+            return View(result);
         }
     }
 }
