e93e060d383115d9ce519dca35f2533644cb2a1f
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-03-31 20:00:41 +0400

    Room with repositories

---
+++ BCTFO-20-NC/HotelProject.Repository/RoomRepository.cs
@@ -0,0 +1,251 @@
+﻿using HotelProject.Data;
+using HotelProject.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace HotelProject.Repository
+{
+    public class RoomRepository
+    {
+        public async Task<List<Room>> GetRooms()
+        {
+            List<Room> result = new();
+            const string sqlExpression = "sp_GetAllRooms";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (await reader.ReadAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            result.Add(new Room()
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = !reader.IsDBNull(1) ? reader.GetString(1) : string.Empty,
+                                IsFree = !reader.IsDBNull(2) ? reader.GetBoolean(2) : false,
+                                HotelId = !reader.IsDBNull(3) ? reader.GetInt32(3) : 0,
+                                DailyPrice = !reader.IsDBNull(4) ? reader.GetDouble(4) : 0
+                            });
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+
+                return result;
+            }
+
+        }
+
+        public async Task<List<Room>> GetRoomsOfHotel(int hotelId)
+        {
+            List<Room> result = new();
+            const string sqlExpression = "sp_GetAllRoomsOfHotel";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("hotelId", hotelId);
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (await reader.ReadAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            result.Add(new Room()
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = !reader.IsDBNull(1) ? reader.GetString(1) : string.Empty,
+                                IsFree = !reader.IsDBNull(2) ? reader.GetBoolean(2) : false,
+                                HotelId = !reader.IsDBNull(3) ? reader.GetInt32(3) : 0,
+                                DailyPrice = !reader.IsDBNull(4) ? reader.GetDouble(4) : 0
+                            });
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+
+                return result;
+            }
+
+        }
+
+        public async Task AddRoom(Room room)
+        {
+            string sqlExpression = "sp_AddRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command = new(sqlExpression, connection);
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("name", room.Name);
+                    command.Parameters.AddWithValue("isFree", room.IsFree);
+                    command.Parameters.AddWithValue("hotelId", room.HotelId);
+                    command.Parameters.AddWithValue("dailyPrice", room.DailyPrice);
+
+                    await connection.OpenAsync();
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+
+        public async Task DeleteRoom(int id)
+        {
+            string sqlExpression = "sp_DeleteRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id", id);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+
+        public async Task UpdateRoom(Room room)
+        {
+            string sqlExpression = "sp_UpdateRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new(sqlExpression, connection);
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("id", room.Id);
+                    command.Parameters.AddWithValue("name", room.Name);
+                    command.Parameters.AddWithValue("isFree", room.IsFree);
+                    command.Parameters.AddWithValue("hotelId", room.HotelId);
+                    command.Parameters.AddWithValue("dailyPrice", room.DailyPrice);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+
+        public async Task<Room> GetSingleRoom(int id)
+        {
+            Room result = new();
+            const string sqlExpression = "sp_GetSingleRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id", id);
+
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (await reader.ReadAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            result.Id = reader.GetInt32(0);
+                            result.Name = !reader.IsDBNull(1) ? reader.GetString(1) : string.Empty;
+                            result.IsFree = !reader.IsDBNull(2) ? reader.GetBoolean(2) : false;
+                            result.HotelId = !reader.IsDBNull(3) ? reader.GetInt32(3) : 0;
+                            result.DailyPrice = !reader.IsDBNull(4) ? reader.GetDouble(4) : 0;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+
+                return result;
+            }
+
+        }
+
+    }
+}
---
+++ BCTFO-20-NC/HotelProject.Tests/RoomShould.cs
@@ -0,0 +1,68 @@
+﻿using HotelProject.Models;
+using HotelProject.Repository;
+
+namespace HotelProject.Tests
+{
+    public class RoomShould
+    {
+        private readonly RoomRepository _roomRepository;
+        public RoomShould()
+        {
+            _roomRepository = new();
+        }
+
+
+        [Fact]
+        public async void Return_All_Rooms_From_Database()
+        {
+            var result = await _roomRepository.GetRooms();
+        }
+
+        [Fact]
+        public async void Return_All_Rooms_Of_Hotel_From_Database()
+        {
+            var result = await _roomRepository.GetRoomsOfHotel(hotelId: 1);
+        }
+
+        [Fact]
+        public async void Add_New_Room_In_Database()
+        {
+            Room newRoom = new()
+            {
+                Name = "C-40",
+                IsFree = true,
+                HotelId = 3,
+                DailyPrice = 150
+            };
+
+            await _roomRepository.AddRoom(newRoom);
+        }
+
+        [Fact]
+        public async void Update_Room_In_Database()
+        {
+            Room room = new()
+            {
+                Id = 28,
+                Name = "C-50",
+                IsFree = true,
+                HotelId = 3,
+                DailyPrice = 150
+            };
+
+            await _roomRepository.UpdateRoom(room);
+        }
+
+        [Fact]
+        public async void Delete_Room_In_Database()
+        {
+            await _roomRepository.DeleteRoom(28);
+        }
+
+        [Fact]
+        public async void Get_Single_Room()
+        {
+            var result = await _roomRepository.GetSingleRoom(28);
+        }
+    }
+}
