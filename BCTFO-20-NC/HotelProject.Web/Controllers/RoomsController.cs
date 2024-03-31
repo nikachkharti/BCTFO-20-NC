88f41178debf4c63147f84fd88bc53cf3bd9e546
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-03-31 20:14:03 +0400

    Rooms in web

---
+++ BCTFO-20-NC/HotelProject.Web/Controllers/RoomsController.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly RoomRepository _roomRepository;
+        public RoomsController()
+        {
+            _roomRepository = new();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _roomRepository.GetRooms();
+            return View(result);
+        }
+    }
+}
