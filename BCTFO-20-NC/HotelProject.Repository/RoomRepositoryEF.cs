3bd5e17296ab4cdd77afae96fdffb8bd5ecf3610
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-04-12 13:56:13 +0400

    Homework

--- BCTFO-20-NC/HotelProject.Repository/RoomRepositoryEF.cs
+++ BCTFO-20-NC/HotelProject.Repository/RoomRepositoryEF.cs
@@ -1,38 +1,111 @@
-﻿using HotelProject.Models;
+﻿using HotelProject.Data;
+using HotelProject.Models;
 using HotelProject.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelProject.Repository
 {
     public class RoomRepositoryEF : IRoomRepository
     {
-        public Task AddRoom(Room room)
+        private readonly ApplicationDbContext _context;
+        public RoomRepositoryEF(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task DeleteRoom(int id)
+        public async Task AddRoom(Room room)
         {
-            throw new NotImplementedException();
+            if (room == null)
+            {
+                throw new ArgumentNullException("Invalid argument passed");
+            }
+
+            await _context.Rooms.AddAsync(room);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<List<Room>> GetRooms()
+        public async Task DeleteRoom(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+            {
+                throw new ArgumentNullException("Invalid argument passed");
+            }
+
+            var entity = await _context.Rooms.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entity == null)
+            {
+                throw new NullReferenceException("Entity not found");
+            }
+
+            _context.Rooms.Remove(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<List<Room>> GetRoomsOfHotel(int hotelId)
+        public async Task<List<Room>> GetRooms()
         {
-            throw new NotImplementedException();
+            var entities = await _context.Rooms
+                .Include(nameof(Hotel))
+                .ToListAsync();
+
+            if (entities == null)
+            {
+                throw new NullReferenceException("Entities not found");
+            }
+
+            return entities;
         }
 
-        public Task<Room> GetSingleRoom(int id)
+        public async Task<List<Room>> GetRoomsOfHotel(int hotelId)
         {
-            throw new NotImplementedException();
+            var entities = await _context.Rooms
+                .Where(x => x.HotelId == hotelId)
+                .Include(nameof(Hotel))
+                .ToListAsync();
+
+            if (entities == null)
+            {
+                throw new NullReferenceException("Entities not found");
+            }
+
+            return entities;
         }
 
-        public Task UpdateRoom(Room room)
+        public async Task<Room> GetSingleRoom(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Rooms
+                .Include(nameof(Hotel))
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entity == null)
+            {
+                throw new NullReferenceException("Entity not found");
+            }
+
+            return entity;
+        }
+
+        public async Task UpdateRoom(Room room)
+        {
+            if (room == null || room.Id <= 0)
+            {
+                throw new ArgumentNullException("Invalid argument passed");
+            }
+
+            var entity = await _context.Rooms.FirstOrDefaultAsync(x => x.Id == room.Id);
+
+            if (entity == null)
+            {
+                throw new NullReferenceException("Entity not found");
+            }
+
+            entity.Name = room.Name;
+            entity.IsFree = room.IsFree;
+            entity.DailyPrice = room.DailyPrice;
+            entity.HotelId = room.HotelId;
+
+            _context.Rooms.Update(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
--- BCTFO-20-NC/HotelProject.Web/Controllers/RoomsController.cs
+++ BCTFO-20-NC/HotelProject.Web/Controllers/RoomsController.cs
@@ -1,21 +1,70 @@
-﻿using HotelProject.Repository;
+﻿using HotelProject.Models;
 using HotelProject.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HotelProject.Web.Controllers
 {
     public class RoomsController : Controller
     {
         private readonly IRoomRepository _roomRepository;
-        public RoomsController(IRoomRepository roomRepository)
+        private readonly IHotelRepository _hotelRepository;
+        public RoomsController(IRoomRepository roomRepository, IHotelRepository hotelRepository)
         {
             _roomRepository = roomRepository;
+            _hotelRepository = hotelRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             var result = await _roomRepository.GetRooms();
             return View(result);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var hotels = await _hotelRepository.GetHotels();
+            ViewBag.Hotels = new SelectList(hotels, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Room model)
+        {
+            await _roomRepository.AddRoom(model);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _roomRepository.GetSingleRoom(id);
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeletePOST(int id)
+        {
+            await _roomRepository.DeleteRoom(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var result = await _roomRepository.GetSingleRoom(id);
+            var hotels = await _hotelRepository.GetHotels();
+            ViewBag.Hotels = new SelectList(hotels, "Id", "Name");
+
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePOST(Room model)
+        {
+            await _roomRepository.UpdateRoom(model);
+            return RedirectToAction("Index");
         }
     }
 }
